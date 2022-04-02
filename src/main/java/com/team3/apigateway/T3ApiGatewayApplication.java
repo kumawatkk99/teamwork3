531bd78eedf1657184832aad3277aa867b9ff2d7
Kushal <kushalkumawat1999@gmail.com>
2022-04-02 17:45:34 +0530

    fc

---
+++ src/main/java/com/team3/apigateway/T3ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.team3.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/apigateway/T3ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
